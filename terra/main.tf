402c9b13bd3326866c6a907380ac52d631dd1fd9
shiv-rajawat <bindassmangal@gmail.com>
2018-11-13 14:17:29 +0530

    Update main.tf

--- terra/main.tf
+++ terra/main.tf
@@ -1,27 +1,24 @@
 # terraform state file setup
 
 provider "aws" {
-  region = "us-east-2"
+  region = "ap-southeast-2"
 }
 
 # create an S3 bucket to store the state file in
 
 resource "aws_s3_bucket" "terraform-state-storage" {
-    bucket = "terraform-remote-state-bucket"
+    bucket = "shiv-terraform-remote-state-bucket"
     versioning {
       enabled = true
     }
     lifecycle {
       prevent_destroy = true
     }
-    tags {
-      Name = "S3 Remote Terraform State Store"
-    }
 }
 
 # create a dynamodb table for locking the state file
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock"
+  name = "shiv-terraform-state-lock"
   hash_key = "LockID"
   read_capacity = 20
   write_capacity = 20
@@ -29,9 +26,5 @@ resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
   attribute {
     name = "LockID"
     type = "S"
-  }
-
-  tags {
-    Name = "DynamoDB Terraform State Lock Table"
   }
 }
