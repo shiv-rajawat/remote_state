67d94c744f15594cb80d017ea2dd06ea0369fd0b
shiv-rajawat <bindassmangal@gmail.com>
2018-11-14 16:49:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
 # terraform state file setup
+variable "access_key" {
+  type = "string"
+  }
+
+variable "secret_key" {
+  type = "string"
+  }
+
 
 provider "aws" {
   region = "ap-southeast-2"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
 # create an S3 bucket to store the state file in
